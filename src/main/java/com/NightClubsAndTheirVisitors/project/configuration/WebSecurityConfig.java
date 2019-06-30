14a6feafac189af4479d991ce05888cebb02ea9b
AndrewUkraine <andrewukraine17@gmail.com>
2019-07-01 00:00:42 +0300

    modify spring security for bootstrap

--- src/main/java/com/NightClubsAndTheirVisitors/project/configuration/WebSecurityConfig.java
+++ src/main/java/com/NightClubsAndTheirVisitors/project/configuration/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements U
 
         http
                 .authorizeRequests()
-                .antMatchers("/","/user/**", "/static/**", "/login/**", "/login", "https://night-clubs-and-visitors.herokuapp.com/login").permitAll()
+                .antMatchers("/","/user/**", "/static/**", "/login/**", "login", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
